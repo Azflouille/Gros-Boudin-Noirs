1a8deae889e74a595f178165ac985a1b09c4cec6
RemiDNancy <remi.dero7@etu.univ-lorraine.fr>
2022-03-10 17:46:15 +0100

    Toute la vie

--- site/dist/js/app.js
+++ site/dist/js/app.js
@@ -5,18 +5,34 @@ import cart from "./cart.js";
 function init(){
     ui.buildProductsList(products.products);
 
-    let sBar = document.querySelector('#product-search');
-    sBar.addEventListener('keyup', (e) => {
-        let sRes = products.search(sBar.value);
-        ui.buildProductsList(sRes);
-    });
-
     let videPanier = document.querySelector('#empty-cart');
     videPanier.addEventListener('click', (e) => {
         cart.emptyCart();
         ui.displayCart();
     });
 
+    let sBar = document.querySelector('#product-search');
+    sBar.addEventListener('keyup', (e) => {
+        let sRes = products.search(sBar.value);
+        ui.buildProductsList(sRes);
+    });
+
+    function openNav() {
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+    }
+
+    function closeNav() {
+        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+    }
+
+    document.querySelector("#btnopen").addEventListener( "click", e => {
+        openNav();
+    });
+    document.querySelector("#btnclose").addEventListener( "click", e => {
+        closeNav();
+    });
 
 
 }
